a7883ce24e446354a9568543b886e424f5ea434e
Nabil Nader <nabil.nader00@gmail.com>
2022-11-01 10:30:06 +0200

    work with sort radix

--- src/com/ex/sort/shell/Shell.java
+++ src/com/ex/sort/shell/Shell.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class Shell {
     public static void main(String[] args) {
-        int [] intArray = {20,35,-15,7,55,1,-22};
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
 
-        for(int gap = intArray.length/2 ; gap > 0; gap /=2 ){
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
 
 
-            for(int i = gap ; i <intArray.length ; i++){
+            for (int i = gap; i < intArray.length; i++) {
                 int newElement = intArray[i];
                 int j = i;
-                 while (j >= gap && intArray[j - gap] > newElement){
-                    intArray[j] = intArray[j-gap];
-                    j -=gap;
-                 }
-                 intArray[j] = newElement;
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
             }
 
 
